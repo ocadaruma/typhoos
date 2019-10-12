5e1a238b600a3a6e460739f04bc1e8085f8a0825
ocadaruma <ocadaruma@gmail.com>
2019-10-12 21:43:26 +0900

    boot

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static WELCOME: &[u8] = b"Typoos";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in WELCOME.iter().enumerate() {
+        unsafe {
+            *vga_buffer.add(i * 2) = byte;
+            *vga_buffer.add(i * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
